0ea89b820dc3f48b5ea6c452ec3d9024acf92d61
jenea130 <jenea130@mail.ru>
2024-03-23 13:17:00 +0200

    feat: home-intro

--- template-parts/home/home-intro.php
+++ template-parts/home/home-intro.php
@@ -1,11 +1,25 @@
 <?php
 $home_intro = get_field('home_intro');
-$title=$home_intro['title'];
-$text=$home_intro['text'];
-$img=$home_intro['img'];
-$file=$home_intro['file'];
-$button=$home_intro['button'];
+$label = $home_intro['label'];
+$title = $home_intro['title'];
+$text = $home_intro['text'];
+$image = $home_intro['image'];
+$button_text = $home_intro['button_text'];
 ?>
 
-<div class="home-intro">
-</div>
+<section class="home-intro">
+  <div class="home-intro__img">
+    <img src="<?php echo $image; ?>" alt="">
+    <div class="container">
+      <div class="home-intro__content">
+        <div class="home-intro__label label"><?php echo $label; ?></div>
+        <h1 class="home-intro__title"><?php echo $title; ?></h1>
+        <div class="home-intro__text"><?php echo $text; ?></div>
+        <a class="btn-icon" href="#">
+          <?php get_template_part('template-parts/icons/icon-book'); ?>
+          <span><?php echo $button_text; ?></span>
+        </a>
+      </div>
+    </div>
+  </div>
+</section>
