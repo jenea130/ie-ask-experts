d1139eba1971d97893c4315dc6617aac54bd6d78
jenea130 <jenea130@mail.ru>
2024-03-25 16:11:57 +0200

    feat: about-me

--- front-page.php
+++ front-page.php
@@ -8,6 +8,8 @@ get_header();
 <?php echo get_template_part('template-parts/home/home-intro'); ?>
 <div class="container">
   <?php echo get_template_part('template-parts/home/features'); ?>
+  <?php echo get_template_part('template-parts/home/about-me'); ?>
 </div>
+<?php echo get_template_part('template-parts/home/services'); ?>
 
 <?php get_footer(); ?>
