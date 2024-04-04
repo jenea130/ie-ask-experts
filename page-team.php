999b34e0d40048e787f05b57dbe326ae827e9d99
jenea130 <jenea130@mail.ru>
2024-04-04 19:19:09 +0300

    feat: contact page-team

--- page-team.php
+++ page-team.php
@@ -9,22 +9,7 @@ pageIntroComponent($page_intro, 'page-intro--team');
     <?php get_template_part('template-parts/team/great-team'); ?>
   </div>
   <div class="container">
-    <div class="contact contact--team">
-      <div class="contact__img"> <img src="assets/i/contact/contact-img.jpg" alt=""></div>
-      <div class="contact__content">
-        <div class="contact__wrap">
-          <div class="contact__label label">Contact</div>
-          <h2 class="contact__title title">Ready To Talk?</h2>
-          <div class="contact__text text">Posuere sollicitudin aliquam ultrices sagittis orci a scelerisque. Massa placerat duis ultricies lacus sed turpis. Pellentesque pellentesque habitant morbi tristique.</div>
-        </div>
-        <div class="contact__form form-small">
-          <div class="form-small__group">
-            <input type="text" placeholder="your mail address">
-            <button class="btn btn--contrast" type="button">Send Message </button>
-          </div>
-        </div>
-      </div>
-    </div>
+    <?php echo get_template_part('template-parts/home/contact'); ?>
   </div>
 
   <?php get_footer(); ?>
--- template-parts/home/contact.php
+++ template-parts/home/contact.php
@@ -1,5 +1,5 @@
-<?php 
-$contact = get_field('contact');
+<?php
+$contact = get_field('contact', 2);
 $label = $contact['label'];
 $title = $contact['title'];
 $text = $contact['text'];
@@ -7,21 +7,25 @@ $image = $contact['image'];
 $placeholder = $contact['placeholder'];
 $button_text = $contact['button_text'];
 ?>
-<div class="contact">
-  <div class="contact__img">
-    <img src="<?php echo $image; ?>" alt="">
-  </div>
-  <div class="contact__content">
-    <div class="contact__wrap">
-      <div class="contact__label label"><?php echo $label; ?></div>
-      <h2 class="contact__title title"><?php echo $title; ?></h2>
-      <div class="contact__text text"><?php echo $text; ?></div>
+<?php if (is_page(392)) : ?>
+  <div class="contact contact--team">
+  <?php else : ?>
+    <div class="contact">
+    <?php endif; ?>
+    <div class="contact__img">
+      <img src="<?php echo $image; ?>" alt="">
     </div>
-    <div class="contact__form form-small">
-      <div class="form-small__group">
-        <input type="text" placeholder="<?php echo $button_text; ?>">
-        <button class="btn btn--contrast" type="button"><?php echo $button_text; ?></button>
+    <div class="contact__content">
+      <div class="contact__wrap">
+        <div class="contact__label label"><?php echo $label; ?></div>
+        <h2 class="contact__title title"><?php echo $title; ?></h2>
+        <div class="contact__text text"><?php echo $text; ?></div>
+      </div>
+      <div class="contact__form form-small">
+        <div class="form-small__group">
+          <input type="text" placeholder="<?php echo $button_text; ?>">
+          <button class="btn btn--contrast" type="button"><?php echo $button_text; ?></button>
+        </div>
       </div>
     </div>
-  </div>
-</div>
+    </div>
