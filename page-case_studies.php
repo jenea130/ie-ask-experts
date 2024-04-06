5fb8e04067d0c3143697800844e3b0ea9d0087ba
jenea130 <jenea130@mail.ru>
2024-04-06 16:48:25 +0300

    feat: page-intro case-studies

--- page-case_studies.php
+++ page-case_studies.php
@@ -1,5 +1,10 @@
 <?php get_header(); ?>
 
+<?php
+$page_intro = get_field('page_intro');
+pageIntroComponent($page_intro, 'page-intro--case-studies');
+?>
+
 <div class="container">
   <?php get_template_part('template-parts/case-studies/taxes'); ?>
   <?php get_template_part('template-parts/services/contact-us'); ?>
