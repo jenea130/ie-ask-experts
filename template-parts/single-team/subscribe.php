42aac16c1e578025ff20800f017c5592b4c4d390
jenea130 <jenea130@mail.ru>
2024-04-10 19:30:25 +0300

    feat: contact-us on page-contact

--- template-parts/single-team/subscribe.php
+++ template-parts/single-team/subscribe.php
@@ -1,25 +1,29 @@
-<?php 
+<?php
 $subscribe = get_field('subscribe', 392);
 $title = $subscribe['title'];
 $text = $subscribe['text'];
 $placeholder = $subscribe['placeholder'];
 $button_text = $subscribe['button_text'];
 ?>
-<div class="subscribe">
-  <div class="subscribe__dotted">
-    <?php get_template_part('template-parts/icons/fon-subscribe'); ?>
-  </div>
-  <div class="subscribe__square"></div>
-  <div class="subscribe__wrapper">
-    <div class="subscribe__content">
-      <h3 class="subscribe__subtitle"><?php echo $title; ?></h3>
-      <div class="subscribe__text text"><?php echo $text; ?></div>
+<?php if (is_page(34)) : ?>
+  <div class="subscribe subscribe--contact">
+  <?php else : ?>
+    <div class="subscribe">
+    <?php endif; ?>
+    <div class="subscribe__dotted">
+      <?php get_template_part('template-parts/icons/fon-subscribe'); ?>
     </div>
-    <div class="subscribe__form form-small">
-      <div class="form-small__group">
-        <input type="text" placeholder="<?php echo $placeholder; ?>">
-        <button class="btn btn--accent" type="button"><?php echo $button_text; ?></button>
+    <div class="subscribe__square"></div>
+    <div class="subscribe__wrapper">
+      <div class="subscribe__content">
+        <h3 class="subscribe__subtitle"><?php echo $title; ?></h3>
+        <div class="subscribe__text text"><?php echo $text; ?></div>
+      </div>
+      <div class="subscribe__form form-small">
+        <div class="form-small__group">
+          <input type="text" placeholder="<?php echo $placeholder; ?>">
+          <button class="btn btn--accent" type="button"><?php echo $button_text; ?></button>
+        </div>
       </div>
     </div>
-  </div>
-</div>
+    </div>
