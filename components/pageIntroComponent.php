37cb79fc10f1bb054b0072a89038e0d80edb5366
jenea130 <jenea130@mail.ru>
2024-04-10 17:49:39 +0300

    feat: single-blog

--- components/pageIntroComponent.php
+++ components/pageIntroComponent.php
@@ -14,13 +14,17 @@ function pageIntroComponent($page_intro, $class = '')
     <img class="page-intro__img" src="<?php echo $image ?>" alt="">
     <div class="container">
       <div class="page-intro__content">
+
         <?php if ($label == '') : ?>
           <div class="label"><?php echo the_time('F j, Y'); ?></div>
         <?php endif; ?>
+
         <div class="label"><?php echo $label; ?></div>
+
         <?php if ($title == '') : ?>
           <h1 class="page-intro__title"><?php echo get_the_title(); ?></h1>
         <?php endif; ?>
+
         <h1 class="page-intro__title"><?php echo $title; ?></h1>
       </div>
     </div>
---
+++ my_temp.php
@@ -0,0 +1,38 @@
+<div class="consulting__block">
+  <h2 class="consulting__title">Best Business Consulting Services</h2>
+  <div class="consulting__text text">Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring. Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps.</div>
+</div>
+<div class="consulting__img">
+  <img src="assets/i/consulting/consulting-1.jpg" alt="">
+</div>
+<div class="consulting__block">
+  <h2 class="consulting__title">The Digital Assets For All</h2>
+  <div class="consulting__text text">Appropriately empower dynamic leadership skills after business portals. Globally my carminate interactive supply chains with distinctive quality vectors global sources services. Uniquely matrix economically sound value through cooperative technology. Competently parallel task fully researched data and enterprise process improvements. </div>
+</div>
+<ul class="consulting__list">
+  <li class="consulting__item">Dynamically target high-payoff intellectual capital for customized</li>
+  <li class="consulting__item">Interactively procrastinate high-payoff content</li>
+  <li class="consulting__item">Credibly reinter mediate backend ideas for cross-platform models</li>
+</ul>
+<div class="consulting__cite">
+  <div class="consulting__dotted">
+    <?php get_template_part('template-parts/icons/fon-consulting'); ?>
+  </div>
+  <div class="consulting__square"></div>
+  <div class="consulting__icon">
+    <svg width="85" height="50" viewBox="0 0 85 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M84.6211 0.226562C83.3841 5.5 82.1146 11.0339 80.8125 16.8281C79.5755 22.6224 78.4362 28.319 77.3945 33.918C76.3529 39.4518 75.474 44.5299 74.7578 49.1523H47.4141L46.0469 47.0039C47.2188 42.2513 48.7161 37.2057 50.5391 31.8672C52.4271 26.4635 54.4453 21.0599 56.5938 15.6562C58.8073 10.1875 61.0208 5.04427 63.2344 0.226562H84.6211ZM39.0156 0.226562C37.7786 5.5 36.5091 11.0339 35.207 16.8281C33.9701 22.6224 32.8307 28.319 31.7891 33.918C30.7474 39.4518 29.8685 44.5299 29.1523 49.1523H1.80859L0.441406 47.0039C1.61328 42.2513 3.11068 37.2057 4.93359 31.8672C6.82161 26.4635 8.83984 21.0599 10.9883 15.6562C13.2018 10.1875 15.4154 5.04427 17.6289 0.226562H39.0156Z" fill="#FFCC4A"></path>
+    </svg>
+  </div>
+  <div class="consulting__subtext">“The job market of the future will consist of those jobs that robots cannot perform. Our blue-collar work is pattern recognition, making sense of what you see. Gardeners will still have jobs because every garden is different.”</div>
+</div>
+<div class="consulting__block">
+  <h2 class="consulting__title">Make real time a wireframes services</h2>
+  <div class="consulting__text text">Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI.</div>
+</div>
+<ol class="consulting__list-1">
+  <li class="consulting__item-1">It brings the right people together with all the right information and tools to get work done</li>
+  <li class="consulting__item-1">We provide operational efficiency, data security, and flexible scale</li>
+  <li class="consulting__item-1">Your best work, together in one package that works seamlessly from your computer</li>
+  <li class="consulting__item-1">Delivers the tools you need to save time Improve field performance always</li>
+</ol>
